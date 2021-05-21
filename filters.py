120c804fa1a5d2ee1479a53cc354f88be60fc19f
justaprudev <justaprudev@gmail.com>
2021-05-21 06:17:41 +0530

    filters: added regex support

--- filters.py
+++ filters.py
@@ -1,6 +1,7 @@
 # This file is distributed as a part of the polygon project (justaprudev.github.io/polygon)
 # By justaprudev
 
+import re
 from pathlib import Path
 
 # db functions
@@ -20,17 +21,17 @@ def get_filters(chat_id) -> dict:
     return filters
 
 
-def add_filter(chat_id, name, content):
+def add_filter(chat_id, key, content):
     filters = get_filters(chat_id)
-    filters[name] = content
+    filters[key] = content
     set_filters(FILTERS_CACHE)
     return True
 
 
-def remove_filter(chat_id, name):
+def remove_filter(chat_id, key):
     filters = get_filters(chat_id)
-    if name in filters:
-        del filters[name]
+    if key in filters:
+        del filters[key]
         set_filters(FILTERS_CACHE)
         return True
     return False
@@ -46,20 +47,28 @@ def clear_filters(chat_id):
 def set_filters(filters: dict):
     db.add(name=NAME, value=filters)
 
+def isfileid(content):
+    return isinstance(content, int)
+
 
 # Constants
 NAME = Path(__file__).stem
 FILTERS_CACHE = cache_filters()
 
-
+@polygon.on()
 @polygon.on(incoming=True)
 async def filter(e):
     chat_id = str(e.chat_id)
-    message = e.text
+    message = e.text.lower()
     if chat_id in FILTERS_CACHE.keys():
-        for name, content in get_filters(chat_id).items():
-            if name in message:
-                if isinstance(content, int):
+        for key, content in get_filters(chat_id).items():
+            # if key in message:
+            if " " in key:
+                pattern = re.compile(key)
+            else:
+                pattern = re.compile(fr"\b{key}\b")
+            if pattern.search(message):
+                if isfileid(content):
                     f = await polygon.get_messages(polygon.user.id, ids=content)
                     await polygon.forward_messages(e.chat_id, f)  # Forwarding is fast
                     return
@@ -69,12 +78,13 @@ async def filter(e):
 @polygon.on(pattern="filter ?(.*);+?(.*)")
 async def addfilter(e):
     chat_id = str(e.chat_id)
-    name = e.pattern_match.group(1)
+    key = e.pattern_match.group(1)
     content = e.pattern_match.group(2)
     reply = await e.get_reply_message()
-    if not name:
+    if not key:
         await e.edit("`What do you want to filter?`")
         return
+    key = key.lower()
     if not content:
         if reply:
             if reply.media:
@@ -86,7 +96,7 @@ async def addfilter(e):
                 await f.reply(
                     f"`[DO NOT DELETE]`\
                     \n**This file was saved by polygon**\
-                    \n**name:** `{name}`\
+                    \n**key:** `{key}`\
                     \n**id:** `{content}`"
                 )
             else:
@@ -94,32 +104,30 @@ async def addfilter(e):
         else:
             await e.edit("`What do you want to filter it with?`")
             return
-    add_filter(chat_id, name, content)
-    await e.edit(f"**Started filtering** `{name}` **in this chat successfully!**")
+    add_filter(chat_id, key, content)
+    await e.edit(f"**Started filtering** `{key}` **in this chat successfully!**")
 
 
 @polygon.on(pattern="stop ?(.*)")
 async def removefilter(e):
     chat_id = str(e.chat_id)
-    name = e.pattern_match.group(1)
-    if not name:
+    key = e.pattern_match.group(1)
+    if not key:
         clear_filters(chat_id)
         await e.edit("**Stopped all filters in this chat successfully!**")
         return
-    if remove_filter(chat_id, name):
-        await e.edit(f"**Stopped filter** `{name}` in this chat **successfully!**")
+    key = key.lower()
+    if remove_filter(chat_id, key):
+        await e.edit(f"**Stopped filter** `{key}` in this chat **successfully!**")
         return
-    await e.edit(f"`{name}` **is not being filtered in this chat!**")
+    await e.edit(f"`{key}` **is not being filtered in this chat!**")
 
 
 @polygon.on(pattern="filters")
 async def filters(e):
     chat_id = str(e.chat_id)
     filters = get_filters(chat_id)
-    message = "**All filters in this chat:\n**"
     if not filters:
-        await e.edit("`There are no filters in this chat!`")
-        return
-    for name in filters:
-        message += f"\n* `{name}`"
+        return await e.edit("`There are no filters in this chat!`")
+    message = "**All filters in this chat:**\n\n" + "\n".join([f"~` {key}`" for key in filters])
     await e.edit(message)
